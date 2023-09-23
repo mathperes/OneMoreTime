9053a2612dd4765a1475b87a60cfc91382f965a1
Helena <Helenalicheski@gmail.com>
2023-09-23 10:31:48 -0300

    .

--- OneMoreTime/Assets/Scripts/PlayerController.cs
+++ OneMoreTime/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,58 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    private Rigidbody playerRb;
+
+    private float inputHorizontal;
+    private float inputVertical;
+
+    public float speed;
+    public float maxSpeed;
+
+    public bool facingRight;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        PlayerMoviment();
+    }
+
+    public void PlayerMoviment()
+    {
+        inputHorizontal = Input.GetAxis("Horizontal");
+        inputVertical = Input.GetAxis("Vertical");
+
+        playerRb.AddForce(Vector3.right * speed * inputHorizontal);
+
+        if (Mathf.Abs(playerRb.velocity.x) > maxSpeed)
+        {
+            playerRb.velocity = new Vector2(Mathf.Sign(playerRb.velocity.x) * maxSpeed, playerRb.velocity.y);
+        }
+
+        playerRb.AddForce(Vector3.forward * speed * inputVertical);
+
+        if (Mathf.Abs(playerRb.velocity.z) > maxSpeed)
+        {
+            playerRb.velocity = new Vector3(playerRb.velocity.x, playerRb.velocity.y, Mathf.Sign(playerRb.velocity.z) * maxSpeed);
+        }
+
+        if (inputHorizontal > 0 && !facingRight || inputHorizontal < 0 && facingRight)
+        {
+            FlipDirection();
+        }
+    }
+
+    void FlipDirection()
+    {
+        facingRight = !facingRight;
+
+        transform.rotation = Quaternion.Euler(0, facingRight ? 0 : 180, 0);
     }
 }
